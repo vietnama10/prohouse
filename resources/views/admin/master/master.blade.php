865a1cce69f0a6224fd817985cf76bb072eac71b
Dung Le <lqudung.hue@gmail.com>
2018-03-12 15:51:53 +0700

    https admin fontawesome

--- resources/views/admin/master/master.blade.php
+++ resources/views/admin/master/master.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-        <base href="{{scure_asset('')}}" >
+        <base href="{{secure_asset('')}}" >
         <!-- VENDOR CSS -->
 
         <!-- Bootstrap -->
