f6c0bd3ea8db66e4c93d5f22023b0b11c5b124d8
Dung Le <lqudung.hue@gmail.com>
2018-03-17 11:56:26 +0700

    product page

--- resources/views/customer/pages/product.blade.php
+++ resources/views/customer/pages/product.blade.php
@@ -11,16 +11,6 @@
 <link rel="stylesheet" href="./public/vendors/desoslider/css/jquery.desoslide.css">
 @endsection
 @section('Main')
-<div id="fb-root"></div>
-<script>(function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id))
-            return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=1925279281046988&autoLogAppEvents=1';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
 <!-- main page -->
 <div class="home-main-wrapper">
     <div id="home-main" class="container">
@@ -70,7 +60,12 @@
                 <p><strong>Address: </strong><span>{{$product->address}}</span></p>
                 <ul class="social">
                     <li>
-                        <div id="shareBtn" class="btn btn-primary clearfix"><i class="fab fa-facebook"></i></div>
+                        
+                        <div class="fb-share-button" data-href="{{Request::fullUrl()}}" data-layout="button" data-size="large" data-mobile-iframe="true">
+                            <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">
+                                Share
+                            </a>
+                        </div>
                     </li>
                 </ul>
             </div>
@@ -125,13 +120,11 @@
 </script>
 
 <!--Facebook-->
-<script>
-    document.getElementById('shareBtn').onclick = function () {
-        FB.ui({
-            method: 'share',
-            display: 'popup',
-            href: '{{Request::fullUrl()}}',
-        }, function (response) {});
-    }
-</script>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=1925279281046988&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 @endsection
