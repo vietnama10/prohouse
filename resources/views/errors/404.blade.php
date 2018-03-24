dd1933d91fc407e4fd6ae6dd851c7a7dd85c4cf3
Dung Le <lqudung.hue@gmail.com>
2018-03-24 13:31:36 +0700

    logo

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends ('customer.master.master')
+@section('title', 'ERROR')
+
+@section('Main')
+<!-- main page -->
+<div class="home-main-wrapper">
+    <div id="home-main" class="container">
+        <div style="height: 300px; margin-top: 50px;">
+            <h2 style="text-align: center">404 ERROR!</h2>
+            <p style="text-align: center">This page dose not found</p>
+        </div>
+    </div>
+</div>
+<!--/main page -->
+@endsection
